3b512f1b594cca9d337beadc03068ffd8ec6fe32
amovikabammu <35526421+amovikabammu@users.noreply.github.com>
2018-01-17 16:47:26 +0530

    oddoreven.C
    ODD OR EVEN

---
+++ oddoreven.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the number"/n);
+scanf("%d,&a");
+if(a%2=0)
+{
+printf("The given number is even"/n);
+else
+{
+printf("The given number is odd"/n);
+}
+return0;
+}
+}
