8da59316cd1805ead5a677398dc47e6d59eec087
amovikabammu <35526421+amovikabammu@users.noreply.github.com>
2018-01-22 16:54:52 +0530

    passitive or negative.c

---
+++ passitive or nagative.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+double number;
+printf("enter the number:");
+scanf("%f",&number);
+if(number<0.0)
+printf("you entered a nagative number");
+else if(number>0.0)
+printf("you entered pasitive number");
+else
+printf("you entered 0");
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.
