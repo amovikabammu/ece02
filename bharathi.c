f822b89f9aa2fad183f2cd973b754eda6b86ed79
amovikabammu <35526421+amovikabammu@users.noreply.github.com>
2018-01-24 15:44:21 +0530

    bharathi.c

---
+++ bharathi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("Enter a number:\n");
+scanf("%d",&num);
+if(num>0)
+printf("%d is a pasitive number\n",num);
+else if(num<0)
+printf("%dis a negative number\n",num);
+else
+printf("the number is zero\n",num);
+}
