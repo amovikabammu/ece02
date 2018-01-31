d4a4019acfb2aed3e5d786926c512e4834a9ff4f
amovikabammu <35526421+amovikabammu@users.noreply.github.com>
2018-01-31 16:57:43 +0530

    mul.c

---
+++ mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long int sum=o,i,n;
+clrscr();
+printf("\nplease given the value of n:");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("\n5*%ld=%ld",i,5*i);
+}
+getch();
+}
