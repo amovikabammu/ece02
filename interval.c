179ac24366673f399ab8d9c3ffbfeeda4537608e
amovikabammu <35526421+amovikabammu@users.noreply.github.com>
2018-01-31 16:36:47 +0530

    interval.c

---
+++ interval.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+int counter;
+printf("odd number between 1 to 100\n");
+count=1;
+while(count<=100)
+{
+printf("%d",count);
+count=count+c;
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
