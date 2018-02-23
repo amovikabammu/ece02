8eedcb16e59635e9a5dda7453a96efe30a5d33c2
amovikabammu <35526421+amovikabammu@users.noreply.github.com>
2018-02-23 15:46:45 +0530

    sum23.c

---
+++ sum23.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int a,b,sum;
+	printf("\nenter the value of a:");
+	scanf("%d",&a);
+	printf("\n enter the value b:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("\nsum=%d",sum);
+}
+ 
