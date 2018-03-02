93c06e7993c68d72baacf8d49a696b0b07198d24
amovikabammu <35526421+amovikabammu@users.noreply.github.com>
2018-03-02 02:59:36 -0800

    binary2.c

---
+++ binary2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int a[100],i;
+	scanf("%d",a);
+	for(i=0;i<8;i++)
+	
+	{
+	if(a[i]='1'&&a[i]='0')
+	{
+		printf("\n**YES***");
+	}
+	else
+	{
+		printf("\n**NO**");
+	}
+  }
+	
+	return 0;
+}
